20a1c57cbb503c93c55c49e2d07780c4d298d88c
Erhan Kabuk <erhankabuk@hotmail.com>
2022-06-28 14:36:27 +0300

    update database for interval>240

--- src/main/java/com/binancecandlestickfetcher/BinanceCandlestickFetcherApplication.java
+++ src/main/java/com/binancecandlestickfetcher/BinanceCandlestickFetcherApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
 
 @SpringBootApplication
 public class BinanceCandlestickFetcherApplication {
@@ -29,17 +32,29 @@ public class BinanceCandlestickFetcherApplication {
             //service.updateData("EOSUSDT",startTime,5,10);
             //service.updateData("EOSUSDT",startTime,30,10);
             //service.updateData("EOSUSDT",startTime,60,10);
-            //service.updateData("EOSUSDT",startTime,240,10);
+
+            service.updateData("EOSUSDT",startTime,1440,1000);
             //service.updateData("EOSUSDT",startTime,1440,10);
 
             //  String lastEndTime = "C:\\Users\\erhan\\IdeaProjects\\BinanceCandlestickFetcher\\SHIBUSDT\\SHIBUSDT-240\\SHIBUSDT-240 2022-06-23";
             // long newStartTime = service.getLastEndTimeAsNewStartTime(lastEndTime);
-            String lastEndTime = "database\\XRPUSDT\\XRPUSDT-1440\\XRPUSDT-1440 2022-06-24";
-            long newStartTime = service.getLastEndTimeAsNewStartTime(lastEndTime);
 
-            // service.updateData("XRPUSDT", newStartTime, 1440, 10);
-            //service.calculateEndTime(240, start.toString(), 1000);
-            service.convertEpochToLocalDateTime(1527465600);
+/*
+           String lastEndTime = "database\\EOSUSDT\\EOSUSDT-1440\\EOSUSDT-1440 2018-05-28";
+            long newStartTime = service.getLastEndTimeAsNewStartTime(lastEndTime);
+            LocalDateTime endTime= LocalDateTime.now();
+            long endT = service.convertLocalDateTimeToEpoch(endTime);
+            System.out.println("Starttime : "+newStartTime);
+            System.out.println("Endtime : "+endT);
+
+
+            while (newStartTime<=endT) {
+                service.updateData("EOSUSDT", newStartTime, 1440, 1000);
+            newStartTime = service.getLastEndTimeAsNewStartTime(lastEndTime);
+            }
+            */
+             //service.calculateEndTime(240, start.toString(), 1000);
+           // service.convertEpochToLocalDateTime(newStartTime);
             System.exit(0);
         } catch (BeansException e) {
             throw new BusinessIntegrityException(e.getMessage());
